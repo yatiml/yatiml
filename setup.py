b6f8b8cf09d59c8e77143e0616798324b8913ade
Lourens Veen <l.veen@esciencecenter.nl>
2018-07-12 14:49:48 +0200

    Initial commit from NLeSC template

---
+++ setup.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+with open('README.rst') as readme_file:
+    readme = readme_file.read()
+
+setup(
+    name='yatiml',
+    version='0.1.0',
+    description="A library for making YAML-based file formats",
+    long_description=readme + '\n\n',
+    author="Lourens Veen",
+    author_email='l.veen@esciencecenter.nl',
+    url='https://github.com/yatiml/yatiml',
+    packages=[
+        'yatiml',
+    ],
+    package_dir={'yatiml':
+                 'yatiml'},
+    include_package_data=True,
+    license="Apache Software License 2.0",
+    zip_safe=False,
+    keywords='yatiml',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Natural Language :: English',
+        "Programming Language :: Python :: 2",
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+    test_suite='tests',
+    setup_requires=[
+        # dependency for `python setup.py test`
+        'pytest-runner',
+        # dependencies for `python setup.py build_sphinx`
+        'sphinx',
+        'recommonmark'
+    ],
+    tests_require=[
+        'pytest',
+        'pytest-cov',
+        'pycodestyle',
+    ],
+    extras_require={
+        'dev':  ['prospector[with_pyroma]', 'yapf', 'isort'],
+    }
+)
---
+++ yatiml/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""Documentation about YAtiML"""
+
+__version__ = '0.1.0'
+
+__author__ = 'Lourens Veen'
+__email__ = 'l.veen@esciencecenter.nl'
