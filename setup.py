1d249db2a246fd38aedad0aed11f1a98756813db
Lourens Veen <l.veen@esciencecenter.nl>
2018-09-27 23:30:09 +0200

    Set version to 0.0.0.develop for this branch

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open('README.rst') as readme_file:
 
 setup(
     name='yatiml',
-    version='0.1.0',
+    version='0.0.0.dev',
     description="A library for making YAML-based file formats",
     long_description=readme + '\n\n',
     author="Lourens Veen",
