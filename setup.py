efb1b700599f74465088f221cb69a406dbce25cd
Lourens Veen <l.veen@esciencecenter.nl>
2022-03-30 11:25:00 +0200

    Update copyright year and package metadata

--- setup.py
+++ setup.py
@@ -29,7 +29,8 @@ setup(
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9'
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10'
     ],
     install_requires=[
         'ruamel.yaml>=0.15.71,<0.17',
