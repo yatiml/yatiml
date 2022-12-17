dd87a9b0926094631cdb9bcfec8cfcbd42d5e2db
Lourens Veen <l.veen@esciencecenter.nl>
2022-12-17 17:50:07 +0100

    Extend ruamel.yaml version compatibility #36 #48

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ setup(
         'Programming Language :: Python :: 3.10'
     ],
     install_requires=[
-        'ruamel.yaml>=0.15.71,<0.17',
+        'ruamel.yaml>=0.15.71,<=0.17.21,!=0.17.5',
         'typing_extensions'
     ]
 )
